087f93b144c095617bb9ab12840dc27ea97fe4b4
Greg Zaitsev <greg.zaitsev@gmail.com>
2019-10-25 12:04:39 +0300

    Initial commit, project created

---
+++ PolkaIndexer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PolkaIndexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ PolkaIndexer/Schema.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace PolkaIndexer
+{
+    enum 
+
+    enum DataType : byte
+    {
+        [Description("varchar[50]")]
+        Address,
+
+
+    }
+
+
+    class Schema
+    {
+        private 
+    }
+}
