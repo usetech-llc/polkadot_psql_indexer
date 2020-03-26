eaf3f5822410b69928b280e048ed5a1747811094
strmv <strmax20@gmail.com>
2020-03-26 16:42:56 +0300

    Dockerfile update for ui

--- polkascan-pre-explorer-gui/src/environments/environment.ts
+++ polkascan-pre-explorer-gui/src/environments/environment.ts
@@ -6,7 +6,7 @@
 
 export const environment = {
   production: false,
-  jsonApiRootUrl: 'http://localhost/',
+  jsonApiRootUrl: 'http://localhost:5000/',
   networkName: 'Kusama',
   networkTokenSymbol: 'KSM',
   networkTokenDecimals: 12
